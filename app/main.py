da7a2ba91e9df9090f02b5e57f145ade77f10cbe
Ivan_Deyev <deyevivan777@mail.ru>
2026-01-07 18:28:13 +0500

    add main file and project structure

---
+++ app/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from app.routers import brands, cars, users
+from fastapi.staticfiles import StaticFiles
+
+
+# Создаём приложение FastAPI
+app = FastAPI(
+    title="FastAPI Автосалон",
+    version="0.1.0",
+    summary="App for car dealership"
+)
+
+# Подключение маршрутов
+app.include_router(brands.router)
+app.include_router(cars.router)
+app.include_router(users.router)
+
+
+# Для обслуживания медиа файлов
+app.mount("/media", StaticFiles(directory="media"), name="media")
+#Теперь любой файл из media/products/ будет доступен по URL: http://localhost:8000/media/products/abc123.jpg
+
+
+# Корневой эндпоинт для проверки
+@app.get("/")
+async def root():
+    """
+    Корневой маршрут, подтверждающий, что API работает.
+    """
+    return {"message": "Добро пожаловать в API автосалона!"}
